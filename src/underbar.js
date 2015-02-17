9e2c3db2cb5a99a2dc9c44c94b6832926cf92c13
Justin Webb <jw@justinwebb.io>
2015-02-16 16:06:25 -0800

    Pass tests for map and pluck functions

--- src/underbar.js
+++ src/underbar.js
@@ -143,6 +143,15 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var results = [];
+    
+    _.each(collection, function (elem, i, list) {
+      var r = iterator.apply(this, [elem, i, list]);
+
+      results.push(r);
+    });
+
+    return results;
   };
 
   /*
